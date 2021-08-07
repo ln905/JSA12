eea1ff6e3d376ae90f415f481f92b76ddb556683
ln905 <80632340+ln905@users.noreply.github.com>
2021-08-07 13:42:47 +0700

    Add files via upload

---
+++ Buoi4/index.js
@@ -0,0 +1,107 @@
+let listTodos = [];
+let id = 0;
+
+//acordion
+$(document).ready(function(){
+  $(".todo-overview").click(function(){
+    $(this).find(".button-delete").animate({
+      width: "toggle"
+    });
+  });
+});
+
+function delSlide(){
+  $(".todo-overview").click(function(){
+    $(this).find(".button-delete").animate({
+      width: "toggle"
+    });
+  });
+}
+
+//create
+$("#button-add").click(function (e) {
+  let newTodo = $("#newTodo").val();
+  
+  const todo = {
+    id: id, 
+    name: newTodo,
+  };
+  
+  listTodos.push(todo);
+  localStorage.setItem("listTodos", JSON.stringify(listTodos));
+  
+  //create a html code
+  let addHTML = `<div class="todo-overview"> 
+    <p>${newTodo}</p> 
+    <button type="button" class="button-delete" onclick='deleteTask(${id})'></button>
+    </div>`
+
+  $("#todo-list").append(addHTML);
+  clearInput();
+  $(".todo-overview").unbind(); 
+  delSlide();
+  pendingTask()
+  id++; 
+});
+
+//clear input
+function clearInput() {
+  $("#idUpdate").val("");
+  $("#newTodo").val("");
+}
+
+//Delete
+function deleteTask(id) {
+  $(".todo-overview").click(function () {
+    $(this).remove(); 
+    $(".todo-overview").unbind()
+    delSlide()
+  });
+  let index = findIndexTask(id);
+  listTodos.splice(index, 1); 
+  localStorage.removeItem("listTodos"); 
+  localStorage.setItem("listTodos", JSON.stringify(listTodos)); 
+  pendingTask()
+}
+
+function findIndexTask(numberId) {
+  let index;
+  for (let i = 0; i < listTodos.length; i++) {
+    if (listTodos[i].id == numberId) {
+      index = i;
+    }
+  }
+  return index;
+}
+
+//Read
+function readTask() {
+  listTodos = localStorage.getItem("listTodos"); 
+  listTodos = JSON.parse(listTodos); 
+  let tasksHTML = "";
+  for (element of listTodos) {
+    console.log(element);
+    tasksHTML += `
+    <div class="todo-overview"> 
+    <p>${element.name}</p> 
+    <button type="button" class="button-delete" onclick='deleteTask(${element.id})'></button>
+    </div>
+    `
+  }
+  document.querySelector("#todo-list").innerHTML = tasksHTML; 
+  pendingTask()
+}
+
+//Thong bao ve so task
+function pendingTask(){
+  document.getElementById('pending-task').innerHTML = 'You have '+listTodos.length+' pending tasks';
+}
+
+//clear all 
+$("#button-clear").click(function(){
+  $(".todo-overview").remove()
+  localStorage.removeItem("listTodos");
+  pendingTask()
+})
+
+readTask();
